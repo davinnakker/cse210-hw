0483a1e2d98468c5dbdcc4732b426d5bd9b16baa
Davin van den Akker <davinnakker@yahoo.com>
2026-04-20 17:58:44 -0600

    finished prep1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Welcome to my piece of practice code!");
+        Console.WriteLine("Please put your name here: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is your age?");
+        // How do i convert this to an int?
+
+
+        Console.WriteLine($"Welcome to class {name}");
     }
 }

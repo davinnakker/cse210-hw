f72b86f2c0bb906fe7a6e55a88aee74c1bf5f381
Davin van den Akker <davinnakker@yahoo.com>
2026-04-21 10:37:37 -0600

    finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+
+        Random randomGenerator = new Random();
+        int numMagic = randomGenerator.Next(1, 11);
+        
+        int guess = 0;
+
+        while (guess != numMagic)
+        {
+            Console.Write("What is your guess? ");
+            string response = Console.ReadLine();
+            guess = int.Parse(response);
+
+            if (numMagic > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (numMagic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }

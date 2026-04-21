e218d97bda88b333294573f981d043174ac27a7f
Davin van den Akker <davinnakker@yahoo.com>
2026-04-20 18:29:35 -0600

    completed prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string gradeAsString = Console.ReadLine();
+        int grade = int.Parse(gradeAsString);
+
+        string letterGrade;
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your grade is an {letterGrade}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you failed.");
+        }
     }
 }

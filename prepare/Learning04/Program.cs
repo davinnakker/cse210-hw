4496f0afa2822bdf8b4fcf4796bea8469fe94a81
Davin van den Akker <davinnakker@yahoo.com>
2026-05-11 09:31:44 -0600

    learning4 complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        Assignment a1 = new Assignment("davin", "programming");
+        a1.GetSummary();
+
+        MathAssignment a2 = new MathAssignment("Davin", "calc", "3.7", "3-5");
+        a2.GetSummary();
+        a2.GetHomeworkList();
+
+        WritingAssignment a3 = new WritingAssignment("Lidia", "english", "The Book of Mormon");
+        a3.GetSummary();
+        a3.GetWritingInformation();
     }
+
 }
